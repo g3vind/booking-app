dbd9c67250034a1e4f4720d7e9681b698a943599
g3vind <g3vind@gmail.com>
2024-01-28 18:16:11 +0530

    design : bottom navigator

---
+++ NOTES.md
@@ -0,0 +1,15 @@
+# React Navigation
+
+`npm install @react-navigation/native`
+
+# React Native Screens & Safe Area Context
+
+`npx expo install react-native-screens react-native-safe-area-context`
+
+# Install Native Stack
+
+`npm install @react-navigation/native-stack`
+
+# Install Bottom Tabs Navigator
+
+`npm install @react-navigation/bottom-tabs`
---
+++ StackNavigator.js
@@ -0,0 +1,99 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import SavedScreen from "./screens/SavedScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import BookingScreen from "./screens/BookingScreen";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+
+const StackNavigator = () => {
+  const Tab = createBottomTabNavigator();
+  const Stack = createNativeStackNavigator();
+
+  function BottomTabs() {
+    return (
+      <Tab.Navigator>
+        {/* ---------------HOME--------------- */}
+        <Tab.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Entypo name="home" size={24} color="black" />
+              ) : (
+                <AntDesign name="home" size={24} color="black" />
+              ),
+          }}
+        />
+        {/* -----------SAVED----------- */}
+        <Tab.Screen
+          name="Saved"
+          component={SavedScreen}
+          options={{
+            tabBarLabel: "Saved",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <AntDesign name="heart" size={24} color="black" />
+              ) : (
+                <AntDesign name="hearto" size={24} color="black" />
+              ),
+          }}
+        />
+        {/* ------------BOOKING---------- */}
+        <Tab.Screen
+          name="Bookings"
+          component={BookingScreen}
+          options={{
+            tabBarLabel: "Bookings",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="notifications" size={24} color="black" />
+              ) : (
+                <Ionicons
+                  name="notifications-outline"
+                  size={24}
+                  color="black"
+                />
+              ),
+          }}
+        />
+        {/* --------PROFILE-------- */}
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Profile",
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="person" size={24} color="black" />
+              ) : (
+                <Ionicons name="person-outline" size={24} color="black" />
+              ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={BottomTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigator;
+
+const styles = StyleSheet.create({});
